64af6c401c9fc818842a6c06127d7c9ed3ddab92
tooshy <amirhamzahbmn@gmail.com>
2025-01-30 12:51:19 +0800

    added filter function and receipt counter

--- main.py
+++ main.py
@@ -15,7 +15,20 @@ def run(playwright: Playwright, downloaded_files):
     page = browser.new_page()
     page.goto(start_url)
 
-    for x in range(00):
+    start_date = '01/01/2024'
+    end_date = '30/01/2025'
+
+    page.get_by_test_id('sale-filter-button').click()
+    page.get_by_placeholder('start').fill(start_date)
+    page.get_by_placeholder('end').fill(end_date)
+    page.get_by_test_id('modal-cta').click()
+
+    filtered_info_bar = page.get_by_test_id('filtered-info-bar').inner_text()
+    match = re.search(r'(?<=from\s)([\d,]+(?:\.\d{1,2})?)(?=\ssales)', filtered_info_bar)
+    
+    receipt_count = match.group(0)
+
+    for x in range(receipt_count):
         sale_row = page.get_by_test_id(f"sale-row-sale-{x}")
         receipt_no = page.get_by_text(re.compile("#", re.IGNORECASE))
 
@@ -41,7 +54,7 @@ def run(playwright: Playwright, downloaded_files):
 
         page.get_by_test_id('close-modal').click()
 
-    time.sleep(2)
+    time.sleep(4)
 
 print("Checking for files already downloaded..")
 download_path = r"./receipts"
