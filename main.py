1a5cd3e1d8a3f19a610eb48fd56c54eacf0b351a
tooshy <amirhamzahbmn@gmail.com>
2025-01-30 18:17:59 +0800

    added refresh functionality, to bypass row limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from playwright.sync_api import sync_playwright, Playwright
+from playwright.sync_api import sync_playwright, Playwright, expect
 import time
+import datetime as dt
 import os
 import re
 
@@ -23,20 +24,65 @@ def run(playwright: Playwright, downloaded_files):
     page.get_by_placeholder('end').fill(end_date)
     page.get_by_test_id('modal-cta').click()
 
+    sale_list_header_bar = page.get_by_test_id('sale-list-header-sale')
+    date_header = page.get_by_text('Date')
+    sale_list_header_bar.locator(date_header).click()
+
     filtered_info_bar = page.get_by_test_id('filtered-info-bar').inner_text()
     match = re.search(r'(?<=from\s)([\d,]+(?:\.\d{1,2})?)(?=\ssales)', filtered_info_bar)
     
-    receipt_count = match.group(0)
+    receipt_count = int(match.group(0))
 
-    for x in range(receipt_count):
+    print(f'{len(downloaded_files)}/{receipt_count}')
+
+    x = 0
+
+    while len(downloaded_files) != receipt_count:
+
+        # print(x)
+        if x == 150:
+            sale_row = page.get_by_test_id(f'sale-row-sale-{x}')
+            date_info = page.get_by_text('/')
+
+            reset_timestamp = sale_row.locator(date_info).inner_text()
+            reset_start_date = re.match(r'^[^,]+', reset_timestamp).group(0)
+
+            reset_start_date = dt.datetime.strptime(reset_start_date, '%d/%m/%y')
+            reset_start_date = dt.datetime.strftime(reset_start_date, '%d%m%Y')
+
+            dir_list = os.listdir(download_path)
+
+            for pdf in range(len(dir_list)):
+                dir_list[pdf] = dir_list[pdf].rstrip('.pdf')
+                dir_list[pdf] = dir_list[pdf].lstrip('receipt-')
+
+            downloaded_files = dir_list
+
+            print(f'{len(downloaded_files)}/{receipt_count}')
+
+            page.reload()
+
+            page.get_by_test_id('sale-filter-button').click()
+            page.get_by_placeholder('start').fill(reset_start_date)
+            page.get_by_placeholder('end').fill(end_date)
+            page.get_by_test_id('modal-cta').click()
+
+            sale_list_header_bar = page.get_by_test_id('sale-list-header-sale')
+            date_header = page.get_by_text('Date')
+            sale_list_header_bar.locator(date_header).click()
+
+            x = 0
+            # time.sleep(10)
+        
+        page.get_by_test_id(f"sale-row-sale-{x}").scroll_into_view_if_needed()
         sale_row = page.get_by_test_id(f"sale-row-sale-{x}")
-        receipt_no = page.get_by_text(re.compile("#", re.IGNORECASE))
 
-        # sale_row.locator(receipt_no)
+        receipt_no = page.get_by_text("#")
 
         receipt_no = sale_row.locator(receipt_no).inner_text()
         
         if receipt_no in downloaded_files:
+            x+=1
             continue
 
         receipt_button = page.get_by_test_id('sale-receipt-button-sale')
@@ -53,6 +99,8 @@ def run(playwright: Playwright, downloaded_files):
         download.save_as("./receipts/" + f'{page.get_by_role('link',name='Download PDF').get_attribute('Download')}.pdf')
 
         page.get_by_test_id('close-modal').click()
+
+        x += 1
 
     time.sleep(4)
 
